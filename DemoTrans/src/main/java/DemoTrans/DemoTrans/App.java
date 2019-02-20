27984420e38aa373e7e86e64e8a3463aeea21da3
soprarepo <47804449+soprarepo@users.noreply.github.com>
2019-02-20 14:10:12 +0800

    SampleProjectUpload
    Upload the project folder

---
+++ DemoTrans/src/main/java/DemoTrans/DemoTrans/App.java
@@ -0,0 +1,13 @@
+package DemoTrans.DemoTrans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
