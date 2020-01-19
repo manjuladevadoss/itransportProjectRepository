cc880096358f91e86f685184186a702cadf6045a
manjulad <manjula.devadoss@ext.soprasteria.com>
2020-01-19 08:21:02 +0800

    commit
    commit

--- workspace/springbootpro/src/main/java/com/sample/controller/StudentViewcontroller.java
+++ workspace/springbootpro/src/main/java/com/sample/controller/StudentViewcontroller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,27 +17,30 @@ import com.sample.dao.StudentDAO;
 import com.sample.main.entity.StudentEntity;
 import com.sample.main.service.StudentServiceImpl;
 
+
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping(value="/rest")
+@RequestMapping(value="/student/api")
 public class StudentViewcontroller {
 	
 	com.sample.dao.StudentDAO sdao = new StudentDAO();
 
 	//Get all Student using GET
-	@GetMapping(value="/partstudent")
+	@GetMapping(path="/partstudent")
 	public com.sample.model.Student getPartStudent(int i) {
 		com.sample.model.Student stu =  sdao.getStudent(i);
 		return stu;
 	} 	 
 	 
 /** List of all Student using GET **/
-	@GetMapping(value="/studentlist")	
+	@GetMapping(path="/studentlist")	
 	public List<com.sample.model.Student> getStudentList() {
 		return sdao.getStudentList();
 	}
 	
 /** Adding Student using POST and mentioned RequestBody too */
-	@PostMapping(value="/addstudent", consumes="application/json", produces="application/json")	
+	@GetMapping(path="/addstudent", consumes="application/json", produces="application/json")	
 	public String addStudent(@RequestBody com.sample.model.Student stu) {
 		sdao.studentList.add(stu);
 		return "student added";
@@ -52,7 +56,6 @@ public class StudentViewcontroller {
 		List<StudentEntity> list = stuService.getStuList();
 		return new ResponseEntity<List<StudentEntity>>(list,new HttpHeaders(), HttpStatus.OK);
 	}
-	
 	
 	
 }
--- workspace/springbootpro/src/main/java/com/sample/main/SpringbootproApplication.java
+++ workspace/springbootpro/src/main/java/com/sample/main/SpringbootproApplication.java
@@ -3,12 +3,10 @@ package com.sample.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @ComponentScan({"com.sample"})
 @SpringBootApplication
 //@Configuration
-
 public class SpringbootproApplication {
 
 	public static void main(String[] args) {
